6773710d703e1dcb6d2d42c25e8fdd64554366db
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-12-19 07:45:43 +0100

    set tab width to 4

--- Buchungstrainer/src/routes/+page.svelte
+++ Buchungstrainer/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Willkommen zum Buchungstrainer</h1>
 <p>
-	Schau die die <a href="https://github.com/titepasile/HauptprojektIDPA">Repository</a> an um mehr zu
-	erfahren.
+    Schau die die <a href="https://github.com/titepasile/HauptprojektIDPA">Repository</a> an um mehr
+    zu erfahren.
 </p>
