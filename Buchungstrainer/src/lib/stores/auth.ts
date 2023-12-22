0b2ee4b8d15cef252d7d12f47a88e53109b42ce3
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-12-22 10:04:42 +0100

    set default user state to undefined

--- Buchungstrainer/src/lib/stores/auth.ts
+++ Buchungstrainer/src/lib/stores/auth.ts
@@ -2,6 +2,6 @@ import type { User } from "@auth0/auth0-spa-js";
 import { writable } from "svelte/store";
 
 export const isAuthenticated = writable<boolean>(false);
-export const user = writable<User | undefined>({});
+export const user = writable<User | undefined>(undefined);
 export const popupOpen = writable<boolean>(false);
 export const error = writable();
