32104f97bce8410be0e19576f9ca5a4b419daa48
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-01-04 11:33:59 +0100

    rename parameter for readability

--- Buchungstrainer/src/lib/database/insertAssignments.ts
+++ Buchungstrainer/src/lib/database/insertAssignments.ts
@@ -2,6 +2,6 @@ import type { Assignment } from "$interfaces";
 import type { ObjectType } from "deta/dist/types/types/basic";
 import assignmentDb from "./assignmentDb";
 
-export default async function insertAssignments(content: Assignment): Promise<ObjectType> {
-    return assignmentDb.insert(content as unknown as ObjectType);
+export default async function insertAssignments(assignment: Assignment): Promise<ObjectType> {
+    return assignmentDb.insert(assignment as unknown as ObjectType);
 }
