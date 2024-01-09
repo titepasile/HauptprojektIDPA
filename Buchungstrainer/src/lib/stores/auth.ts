db8f33910b85a9f18a9d88ae097306854e994cb6
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2024-01-09 09:28:40 +0100

    auth is now stored in sessionstorage

--- Buchungstrainer/src/lib/stores/auth.ts
+++ Buchungstrainer/src/lib/stores/auth.ts
@@ -1,7 +1,31 @@
 import type { User } from "@auth0/auth0-spa-js";
+import { browser } from '$app/environment';
 import { writable } from "svelte/store";
 
-export const isAuthenticated = writable<boolean>(false);
-export const user = writable<User | undefined>(undefined);
-export const popupOpen = writable<boolean>(false);
+// from here: https://rodneylab.com/using-local-storage-sveltekit/
+const initialIsAuthenticated = browser ? JSON.parse(sessionStorage.getItem("isAuthenticated") ?? "false") : false;
+const initialUser = browser ? JSON.parse(sessionStorage.getItem("user") ?? "undefined") : undefined;
+const initialPopupOpen = browser ? JSON.parse(sessionStorage.getItem("popupOpen") ?? "false") : false;
+
+export const isAuthenticated = writable<boolean>(initialIsAuthenticated);
+export const user = writable<User | undefined>(initialUser);
+export const popupOpen = writable<boolean>(initialPopupOpen);
 export const error = writable();
+
+isAuthenticated.subscribe((value) => {
+    if (browser) {
+        sessionStorage.setItem("isAuthenticated", JSON.stringify(value));
+    }
+});
+
+user.subscribe((value) => {
+    if (browser) {
+        sessionStorage.setItem("user", JSON.stringify(value));
+    }
+});
+
+popupOpen.subscribe((value) => {
+    if (browser) {
+        sessionStorage.setItem("popupOpen", JSON.stringify(value));
+    }
+});
